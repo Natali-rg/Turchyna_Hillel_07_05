3100e03ae205a4f33f968a1ac2cb622ca14c7d69
xx4nd3rr <turchyn.sasha@gmail.com>
2026-08-07 16:41:09 +0300

    Homework25_07.08 update

--- Lessons/homework25/homework25.1.py
+++ Lessons/homework25/homework25.1.py
@@ -27,18 +27,23 @@ def login(driver):
 
         logger.info("Логування пройшло успішно!")
 
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//a[text()=' Log out ']"))).click()
+        logger.info("Розлогування успішне!")
+
+        wait = WebDriverWait(driver, 10)
+
+
     except Exception as e:
         logger.error(f"Помилка під час логування: {e}")
 
     finally:
-        driver.quit()
+        #driver.quit()
         logger.info("Браузер закритий")
 
 login(driver)
 
 
 # XPath локатори
-
 element_XPath1_Home = "//a[text()='Home']"
 element_XPath2_Abput = "//button[text()='About']"
 element_XPath3_Contacts = "//button[text()='Contacts']"
@@ -64,6 +69,44 @@ element_XPath22_Title_Contacts = "//h2[text()='Contacts']"
 element_XPath23_facebook = "//a[@href='https://www.facebook.com/Hillel.IT.School']"
 element_XPath24_email = "//a[@href='mailto:developer@ithillel.ua']"
 element_XPath25_TextFooter = "//footer//p[contains(text(),'Hillel auto developed')]"
+
+
+def check_XPath_elements():
+    elements = [
+        ("Home", "//a[text()='Home']"),
+        ("About", "//button[text()='About']"),
+        ("Contacts", "//button[text()='Contacts']"),
+        ("Guest log in", "//button[text()='Guest log in']"),
+        ("Sign In", "//button[text()='Sign In']"),
+        ("QAuto logo link", "//a[@href='/']"),
+        ("Header logo", "//a[@class='header_logo']"),
+        ("Header Sign In button", "//button[@class='btn btn-outline-white header_signin']"),
+        ("Header nav", "//nav[@class='header_nav d-flex align-items-center']"),
+        ("Header", "//header[@class='header bg-basic-dark']"),
+        ("Header Home", "//header//a[text()='Home']"),
+        ("Header About", "//header//button[text()='About']"),
+        ("Do more", "//div[@class='hero-descriptor']//h1"),
+        ("Do more text", "//h1[text()='Do more!']"),
+        ("Sign up button", "//button[text()='Sign up']"),
+        ("Sign up button class", "//div[@class='hero-descriptor']//button"),
+        ("YouTube iframe", "//iframe[@class='hero-video_frame']"),
+        ("About section", "//div[@id='aboutSection']"),
+        ("Contacts section", "//div[@id='contactsSection']"),
+        ("Fuel expenses", "//p[text()='Log fuel expenses']"),
+        ("Instructions", "//p[text()='Instructions and manuals']"),
+        ("Contacts title", "//h2[text()='Contacts']"),
+        ("Facebook", "//a[@href='https://www.facebook.com/Hillel.IT.School']"),
+        ("Email", "//a[@href='mailto:developer@ithillel.ua']"),
+        ("Footer text", "//footer//p[contains(text(),'Hillel auto developed')]")
+    ]
+    try:
+
+        for name, xpath in elements:
+            element = driver.find_element(By.XPATH, xpath)
+            assert element.is_displayed(), f"Element '{name}' is not displayed"
+            print("Element xpath is displayed")
+    except Exception as e:
+        logger.error(f"Element xpath not found: {e}")
 
 # CSS локатори
 
@@ -92,4 +135,47 @@ element_CSS22_icon_facebook = ".icon-facebook"
 element_CSS23_icon_telegram = ".icon-telegram"
 element_CSS24_footer = ".footer"
 element_CSS25_footer_logo = ".footer_logo"
+
+def check_css_elements():
+
+    elements = [
+        ("Logo", "a.header_logo"),
+        ("Navigation", ".header_nav"),
+        ("Home", ".header_nav a"),
+        ("Home link", ".header-link"),
+        ("Guest login", ".header-link.-guest"),
+        ("Sign in", ".header_signin"),
+        ("Sign in button", "button.header_signin"),
+        ("Header", "header.bg-basic-dark"),
+        ("Header logo", "header .header_logo"),
+        ("Header buttons", "header nav button"),
+        ("Descriptor title", ".hero-descriptor_title"),
+        ("Description", ".hero-descriptor_descr"),
+        ("Sign up button", ".hero-descriptor_btn"),
+        ("Video frame", ".hero-video_frame"),
+        ("YouTube video frame", "iframe.hero-video_frame"),
+        ("About section", "#aboutSection"),
+        ("Contacts section", "#contactsSection"),
+        ("About block title", ".about-block_title"),
+        ("Pictures", ".about-picture_img"),
+        ("Social buttons", ".contacts_socials a"),
+        ("Social link", ".socials_link"),
+        ("Facebook icon", ".icon-facebook"),
+        ("Telegram icon", ".icon-telegram"),
+        ("Footer", ".footer"),
+        ("Footer logo", ".footer_logo")
+    ]
+
+    try:
+        for name, css_selector in elements:
+            element = driver.find_element(By.CSS_SELECTOR, css_selector)
+            assert element.is_displayed(), f"Element '{name}' is not displayed"
+            print("Element css_selector is displayed")
+    except Exception as e:
+        logger.error(f"Element css_selector not found: {e}")
+
+
+check_XPath_elements()
+check_css_elements()
+driver.quit()
 
